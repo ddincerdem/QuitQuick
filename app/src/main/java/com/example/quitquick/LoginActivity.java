458b8520b50d954d5390dd224d1e7e44440196fb
Doğan Dinçer DEMİRCİ <ddincerdem@gmail.com>
2021-03-26 20:37:35 +0300

    LoginActivity
    It will be tried

--- app/src/main/java/com/example/quitquick/LoginActivity.java
+++ app/src/main/java/com/example/quitquick/LoginActivity.java
@@ -2,13 +2,47 @@ package com.example.quitquick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mail =(EditText)findViewById(R.id.txtEmail);
+    EditText password =(EditText)findViewById(R.id.txtPassword);
+    Button login=(Button)findViewById(R.id.btnLogin);
+    Button register=(Button)findViewById(R.id.btnRegister);
+    Intent intentHome = new Intent(this,HomeActivity.class);
+    Intent intentRegister = new Intent(this,RegisterActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SetupClickListener();
+        getSupportActionBar().hide();
+    }
+
+    public void SetupClickListener(){
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(intentHome);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              openActivity(intentRegister);
+            }
+        });
+    }
+
+    public void openActivity(Intent intent){
+
+        startActivity(intent);
     }
 }
